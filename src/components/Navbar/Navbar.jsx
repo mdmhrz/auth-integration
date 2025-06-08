f2ad442a97d733fbdbb443d9986523282b5b4b02
mdmhrz <razufreelance@gmail.com>
2025-06-08 12:47:00 +0600

    Firebase done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router';
+import './Navbar.css'
 
 const Navbar = () => {
     const links = <>
@@ -7,6 +8,7 @@ const Navbar = () => {
         <li> <NavLink to="/login">Login</NavLink> </li>
         <li> <NavLink to="/register">Register</NavLink> </li>
     </>
+
 
     return (
         <div className="navbar bg-base-100 shadow-sm">
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,16 +1,35 @@
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 import React from 'react';
 import { Link } from 'react-router';
+import { auth } from '../../firebase.init';
 
 const Register = () => {
+
+    const handleRegister = e => {
+        e.preventDefault()
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(name, email, password);
+
+        //create user with email & password
+        createUserWithEmailAndPassword(auth, email, password).then(result => {
+            console.log(result);
+        }).catch(error => {
+            console.log(error);
+        })
+    }
     return (
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto mt-10">
-            <h1 className="text-3xl text-center font-bold">Login now!</h1>
+            <h1 className="text-3xl text-center font-bold">Register now!</h1>
             <div className="card-body">
-                <form className="fieldset">
+                <form onSubmit={handleRegister} className="fieldset">
+                    <label className="label">Name</label>
+                    <input type="text" className="input" placeholder="Your Name" name='name' />
                     <label className="label">Email</label>
-                    <input type="email" className="input" placeholder="Email" />
+                    <input type="email" className="input" placeholder="Email" name='email' />
                     <label className="label">Password</label>
-                    <input type="password" className="input" placeholder="Password" />
+                    <input type="password" className="input" placeholder="Password" name='password' />
                     <div><a className="link link-hover">Forgot password?</a></div>
                     <button className="btn btn-neutral mt-4">Login</button>
                 </form>
